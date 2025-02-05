01bf453d63f72f686b3d3995b427f97a89f84697
tauhid97k <tauhid.coder@gmail.com>
2025-02-05 12:42:25 +0600

    added shadcn/ui command component, full ui update, and more.
    - Add shadcn/ui command
    - Modify command component to support floating command list
    - Update vanilla input to command input
    - Get api results and render conditionally with various checking in place

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,16 @@ import SearchInput from "./search-input";
 
 const HomePage = () => {
   return (
-    <main className="h-screen py-4 grid place-items-center grainy">
-      <SearchInput />
+    <main className="h-screen py-4 grainy grid place-items-center">
+      <div className="flex flex-col gap-6 items-center duration-500 animate animate-in fade-in slide-in-from-bottom-2.5">
+        <h1 className="text-5xl font-bold text-zinc-700">SpeedSearch⚡</h1>
+        <p className="text-center max-w-prose text-lg text-zinc-600 leading-snug">
+          A high-performance API built with Hono, Next.js, Redis and Cloudflare.
+          <br />
+          Start typing a country name below to see the results
+        </p>
+        <SearchInput />
+      </div>
     </main>
   );
 };
--- app/search-input.tsx
+++ app/search-input.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import { useEffect, useState } from "react";
 
 const SearchInput = () => {
@@ -15,17 +23,47 @@ const SearchInput = () => {
       if (!input) return setSearchResults(undefined);
 
       const response = await fetch(`/api/search?q=${input}`);
+      const data = await response.json();
+      setSearchResults(data);
     };
 
     fetchResults();
   }, [input]);
 
   return (
-    <input
-      type="text"
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-    />
+    <div className="w-full max-w-md">
+      <Command>
+        <CommandInput
+          value={input}
+          onValueChange={setInput}
+          placeholder="Search countries..."
+          className="placeholder:text-zinc-500"
+        />
+
+        <CommandList>
+          {searchResults?.results.length === 0 && !input ? (
+            <CommandEmpty>No results found.</CommandEmpty>
+          ) : null}
+
+          {searchResults?.results.length ? (
+            <CommandGroup heading="Results">
+              {searchResults?.results.map((result) => (
+                <CommandItem key={result} value={result} onSelect={setInput}>
+                  {result}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
+
+          {searchResults?.results.length ? (
+            <p className="p-2 text-xs text-zinc-500 border-t border-zinc-200">
+              Found {searchResults.results.length} results in&nbsp;
+              {searchResults?.duration.toFixed(0)}ms
+            </p>
+          ) : null}
+        </CommandList>
+      </Command>
+    </div>
   );
 };
 
