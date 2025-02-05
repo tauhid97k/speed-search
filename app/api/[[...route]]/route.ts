640e4b6fffa0b4ad492c0c3857b70a5eefae204e
tauhid97k <tauhid.coder@gmail.com>
2025-02-05 10:38:56 +0600

    search api implementation with redis

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,14 +1,66 @@
+import { Redis } from "@upstash/redis";
 import { Hono } from "hono";
+import { env } from "hono/adapter";
 import { handle } from "hono/vercel";
 
 export const runtime = "edge";
 
-const app = new Hono().basePath("/api");
+type Bindings = {
+  UPSTASH_REDIS_REST_URL: string;
+  UPSTASH_REDIS_REST_TOKEN: string;
+};
 
-app.get("/", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
+
+app.get("/search", async (c) => {
+  try {
+    const { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = env(c);
+
+    // --- Performance Start
+    const performanceStart = performance.now();
+
+    const redis = new Redis({
+      url: UPSTASH_REDIS_REST_URL,
+      token: UPSTASH_REDIS_REST_TOKEN,
+    });
+
+    const query = c.req.query("q")?.toUpperCase();
+
+    if (!query) {
+      return c.json({ message: "Invalid search query" }, 400);
+    }
+
+    const response = [];
+    const rank = await redis.zrank("terms", query);
+
+    if (rank) {
+      const temp = await redis.zrange<string[]>("terms", rank, rank + 100);
+
+      for (const str of temp) {
+        if (!str.startsWith(query)) {
+          break;
+        }
+
+        if (str.endsWith("*")) {
+          response.push(str.substring(0, str.length - 1));
+        }
+      }
+    }
+
+    // --- Performance End
+    const performanceEnd = performance.now();
+
+    return c.json({
+      results: response,
+      duration: performanceEnd - performanceStart,
+    });
+  } catch (error) {
+    console.error(error);
+    return c.json({
+      results: [],
+      message: "Something went wrong!",
+    });
+  }
 });
 
 export const GET = handle(app);
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SearchInput from "./search-input";
 
 const HomePage = () => {
   return (
-    <main className="h-screen py-4 grid place-items-center">
+    <main className="h-screen py-4 grid place-items-center grainy">
       <SearchInput />
     </main>
   );
